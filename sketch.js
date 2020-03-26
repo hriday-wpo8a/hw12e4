6f1c9d01418054148b6f1f6a08db3a3faf318230
hriday-wpo8a <58740006+hriday-wpo8a@users.noreply.github.com>
2020-03-26 15:10:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(800,400);
+  var pillar = createSprite(300, 200, 30, 120);
+  var pillar = createSprite(351, 230, 110, 60);
+  var pillar = createSprite(400, 200, 30, 120);
+  var pillar = createSprite(350, 130, 130, 30);
+  var pillar = createSprite(440, 165, 30, 190);
+  var pillar = createSprite(440, 165, 30, 190);
+  var pillar = createSprite(260, 165, 30, 190);
+
+ 
+}
+
+function draw() {
+  background(255,255,255);
+   // beginShape();
+   // vertex(440,165);
+    //vertex(165,30);
+   // vertex(440,30);
+    //endShape();
+  drawSprites();
+}
